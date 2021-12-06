7b02f1072861597972fca94f442b9e2baba03b58
MUHAMMAD FAIZ BIN ANUAR 198921 <198921@student.upm.edu.my>
2021-12-07 00:52:32 +0800

    update reminder

--- lib/screens/event_editing_page.dart
+++ lib/screens/event_editing_page.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:Ashisu/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:Ashisu/models/event.dart';
 import 'package:provider/provider.dart';
@@ -14,8 +17,68 @@ class EventEditingPage extends StatefulWidget {
 }
 
 class _EventEditingPageState extends State<EventEditingPage> {
+  final _formKey = GlobalKey<FormState>();
+  DateTime fromDate;
+  DateTime toDate;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.event == null) {
+      fromDate = DateTime.now();
+      toDate = DateTime.now().add(Duration(hours: 2));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber[200],
+        leading: CloseButton(color: kPrimaryColor),
+        actions: buildEditingActions(),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              buildTitle(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildEditingActions() {
+    return [
+      ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.transparent,
+          shadowColor: Colors.transparent,
+        ),
+        onPressed: () {},
+        icon: Icon(
+          Icons.done,
+          color: kPrimaryColor,
+        ),
+        label: Text(
+          'Save',
+          style: TextStyle(color: kPrimaryColor),
+        ),
+      ),
+    ];
+  }
+
+  Widget buildTitle() {
+    return TextFormField(
+      style: TextStyle(fontSize: 24),
+      decoration: InputDecoration(
+        border: UnderlineInputBorder(),
+      ),
+      onFieldSubmitted: (_) {},
+    );
   }
 }
