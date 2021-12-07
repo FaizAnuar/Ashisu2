9542464c0e88d045e032cc9a1993fc979851b06d
MUHAMMAD FAIZ BIN ANUAR 198921 <198921@student.upm.edu.my>
2021-12-07 23:48:00 +0800

    Reminder update

--- lib/screens/event_editing_page.dart
+++ lib/screens/event_editing_page.dart
@@ -4,6 +4,7 @@ import 'package:Ashisu/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:Ashisu/models/event.dart';
 import 'package:provider/provider.dart';
+import 'package:Ashisu/utils.dart';
 
 class EventEditingPage extends StatefulWidget {
   final Event event;
@@ -18,6 +19,7 @@ class EventEditingPage extends StatefulWidget {
 
 class _EventEditingPageState extends State<EventEditingPage> {
   final _formKey = GlobalKey<FormState>();
+  final titleController = TextEditingController();
   DateTime fromDate;
   DateTime toDate;
 
@@ -28,6 +30,12 @@ class _EventEditingPageState extends State<EventEditingPage> {
       fromDate = DateTime.now();
       toDate = DateTime.now().add(Duration(hours: 2));
     }
+  }
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    super.dispose();
   }
 
   @override
@@ -45,6 +53,10 @@ class _EventEditingPageState extends State<EventEditingPage> {
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               buildTitle(),
+              SizedBox(
+                height: 12,
+              ),
+              buildDateTimePickers(),
             ],
           ),
         ),
@@ -77,8 +89,51 @@ class _EventEditingPageState extends State<EventEditingPage> {
       style: TextStyle(fontSize: 24),
       decoration: InputDecoration(
         border: UnderlineInputBorder(),
+        hintText: '  Add An Event ',
       ),
       onFieldSubmitted: (_) {},
+      validator: (title) {
+        if (title == null || title.isEmpty) {
+          return 'Title cannot be empty';
+        }
+        return null;
+      },
+      controller: titleController,
+    );
+  }
+
+  buildDateTimePickers() {
+    return Column(
+      children: [buildFrom()],
+    );
+  }
+
+  Widget buildFrom() {
+    return Row(
+      children: [
+        Expanded(
+          child: buildDropdownField(
+            text: Utils.toDate(fromDate),
+            onClicked: () {},
+          ),
+        ),
+        Expanded(
+          child: buildDropdownField(
+            text: Utils.toTime(fromDate),
+            onClicked: () {},
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildDropdownField({
+    String text,
+    VoidCallback onClicked,
+  }) {
+    return ListTile(
+      title: Text(text),
+      trailing: Icon(Icons.arrow_drop_down),
     );
   }
 }
